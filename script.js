2744c78d7bee80ad2c32d5e3f9365f0c72f0ecc9
Bronkgameplays <111917476+Bronkgameplays@users.noreply.github.com>
2023-06-18 22:23:35 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("photoshop");
+        habilidades[3].classList.add("wordpress");
+        habilidades[4].classList.add("drupal");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("proyect");
+    }
+}
+
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+} 
+
+window.onload = function() {
+    var button = document.getElementById('whatsapp-button');
+    button.addEventListener('click', function() {
+      var phoneNumber = '1234567890'; // Reemplaza con tu número de teléfono de WhatsApp
+      var message = '¡Hola! Estoy interesado/a en tus productos/servicios. ¿Podemos hablar?'; // Personaliza el mensaje que se enviará
+  
+      var url = 'https://api.whatsapp.com/send?phone=' + phoneNumber + '&text=' + encodeURIComponent(message);
+      window.open(url, '_blank');
+    });
+  };
+  
